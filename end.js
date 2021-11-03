89c178819a02443cbf4e826c5ba7afbbdb9642b9
BethanyFolino <bethany.folino@gmail.com>
2021-11-02 18:25:23 -0700

    Completed item 10 of Build a Quiz App course: loading and displaying high scores from local storage

--- end.js
+++ end.js
@@ -20,7 +20,7 @@ saveHighScore = (e) => {
         name: username.value
     };
     highScores.push(score);
-    highScores.sort( (a,b) => b.score - a.score);
+    highScores.sort((a,b) => b.score - a.score);
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));

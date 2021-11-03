d5a233aa4281f4544add632ece30e0a6a48bccb7
BethanyFolino <bethany.folino@gmail.com>
2021-11-02 18:05:17 -0700

    Completed item 9 of Build a Quiz App course: now saving high scores in local storage

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -11,4 +14,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = (e) => {
     console.log("clicked save button");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 };
